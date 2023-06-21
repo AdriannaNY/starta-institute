33a2b40a7fcdfac8d7bb3179ef7397690f4ac771
AdriannaNY <dubrova.ekaterina060516@yandex.ru>
2023-06-21 16:33:24 +0300

    Homework3

---
+++ Algorithms/Homework3/src/main/java/org/example/Tower.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Tower {
+        public static void main(String[] args) {
+            int nDisks = 3;
+            doTowers(nDisks, 'A', 'B', 'C');
+        }
+        public static void doTowers(int topN, char from, char inter, char to) {
+            if (topN == 1) {
+                System.out.println("Диск 1 от " + from + " до " + to);
+            } else {
+                doTowers(topN - 1, from, to, inter);
+                System.out.println("Диск " + topN + " от " + from + " до " + to);
+                doTowers(topN - 1, inter, from, to);
+            }
+        }
+}
