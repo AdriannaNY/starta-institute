e0f50f4fcd1513aadba7e367795ba93ecd3e5f71
Ekaterina Dubrova <94081153+AdriannaNY@users.noreply.github.com>
2023-10-24 17:46:35 +0200

    Add files via upload
    Homework10

---
+++ Algorithms/Homework10/src/Main.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class Main {
+    //    У вас есть список активностей, каждая из которых имеет начальное и конечное время.
+//    Ваша цель - выбрать максимальное количество неперекрывающихся активностей.
+//
+//    Напишите функцию maxActivities(activities), которая принимает список активностей.
+//    Каждая активность представлена в виде кортежа (начало, конец).
+//    Функция должна вернуть список индексов выбранных активностей.
+//
+//    Функция должна выбирать активности таким образом,
+//    чтобы ни одна из выбранных активностей не перекрывалась по времени,
+//    и при этом максимизировать общее количество выбранных активностей.
+
+//    Пример:
+//    activities = [(1, 4), (3, 5), (0, 6), (5, 7), (3, 8), (5, 9), (6, 10), (8, 11)]
+//    print(maxActivities(activities))  # Результат: [0, 3, 7]
+
+    public static void main(String[] args) {
+        int[][] activities = {
+                {1, 4}, {3, 5}, {0, 6}, {5, 7}, {3, 8}, {5, 9}, {6, 10}, {8, 11}
+        };
+        for (int[] act : maxActivities(activities)) {
+            System.out.println(Arrays.toString(act));
+        }
+    }
+
+    public static ArrayList<int[]> maxActivities(int[][] arr) {
+        Arrays.sort(arr, (a, b) -> Integer.compare(a[1], b[1]));
+        ArrayList<int[]> picked = new ArrayList<>();
+        picked.add(arr[0]);
+        int end = arr[0][1];
+        for (int[] act : arr) {
+            if (act[0] >= end) {
+                picked.add(act);
+                end = act[1];
+            }
+        }
+        return picked;
+
+    }
+}
